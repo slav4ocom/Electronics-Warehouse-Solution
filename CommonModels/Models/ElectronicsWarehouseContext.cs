56f8034c03ea580fe428da3f7b85dced08ac914f
slav4ocom <slav4o_1@abv.bg>
2021-03-21 03:41:03 +0300

    .

--- CommonModels/Models/ElectronicsWarehouseContext.cs
+++ CommonModels/Models/ElectronicsWarehouseContext.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Console_Manager.Models
+namespace CommonModels
 {
     public class ElectronicsWarehouseContext : DbContext
     {
